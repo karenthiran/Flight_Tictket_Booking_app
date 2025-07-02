86ef135cb73203a5c1bc062c267ef2dd9d547f71
karenthiran <kkarenthiran@gmail.com>
2025-07-03 01:20:48 +0530

    booking section added

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,10 @@
+import FlightSearchcard from "./FlightSearchcard";
 
 export const Main = () => {
   return (
-    <div>Main</div>
+    <div className="col-md-8 col-lg-8 col-xl-8 col-sm-12">
+      <FlightSearchcard/>
+    </div>
   )
 }
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
+import Main from "../components/Main";
 import { Sidebar } from "../components/Sidebar";
 
 export const Homepage = () => {
   return (
     <div>
         {/* sidebar */}
+        <div className="d-flex gap-3">
         <Sidebar/>
+        <div className="main-container">
+          <Main/>
+        </div>
+        </div>
     {/* main */}
+    
 
     {/* filter */}
     </div>
